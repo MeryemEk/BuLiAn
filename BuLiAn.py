9a531435028b86311a12688ecb2daa43d3770da2
MeryemEk <43269599+MeryemEk@users.noreply.github.com>
2022-03-16 19:14:56 +0100

    Update BuLiAn.py

--- BuLiAn.py
+++ BuLiAn.py
@@ -583,7 +583,8 @@ resultat=your_index(selected_region,selected_products,ratios).sort_values(by='Da
 
 
 with row5_2:
-    st.line_chart(resultat)
+    st.line_chart(resultat, width=0, height=0, use_container_width=True)
+    
     
 
 ### TEAM ###
